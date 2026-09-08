ff59de0c2d31030f94585d78164ed103319b5312
Jonah Spector <jonah.m.spector@proton.me>
2026-09-08 15:23:44 -0600

    Fixed arrayDatastore make with vertcat

--- makeArrayDatastores.m
+++ makeArrayDatastores.m
@@ -4,6 +4,13 @@ raw_readall_train = readall(train_ds);
 raw_readall_val = readall(val_ds);
 raw_readall_test = readall(test_ds);
 
+% Make cell array of cell arrays into 2D cell array so output of read() is
+% 1x2 cell array and not cell array of cell arrays.
+
+raw_readall_train = vertcat(raw_readall_train{:});
+raw_readall_val = vertcat(raw_readall_val{:});
+raw_readall_test = vertcat(raw_readall_test{:});
+
 train_ads = arrayDatastore(raw_readall_train, ...
     IterationDimension=1, ...
     OutputType="same");
